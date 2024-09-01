4522398fe06c0e674d791d0a313107706ab55a8a
alexandra_schuszler <schuszleralexandra74@gmail.com>
2024-08-31 23:56:10 -0300

    entregafinal

---
+++ eventos.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded" , function(){
+    let div = document.getElementById("eldiv");
+    div.addEventListener('click', function(){
+        alert( "Hola! Soy el div");
+    });
+}
+);
